6ebebb8f57b41f412b57d3180a75153a99f8b38b
MAT06mat Desktop <mat06mat22@gmail.com>
2025-06-29 16:09:57 +0200

    feat: Refactor PanelMovesList to enhance move handling and scrolling behavior

--- src/game/BoardPanel/Components/PanelMovesList.tsx
+++ src/game/BoardPanel/Components/PanelMovesList.tsx
@@ -4,22 +4,41 @@ import "../../../styles/PanelMovesList.scss";
 import getChessNotation from "../../../utils/getChessNotation";
 import BoardActions from "./BaordActions";
 
-function createPairMoves(arr: string[]) {
-    const result: { m: string | null; i: number }[][] = [];
+interface customMove {
+    move: string | null;
+    index: number;
+    className: string;
+    selected: boolean;
+}
+
+function createCustomMove(
+    move: string | null,
+    index: number,
+    actualMove: number
+) {
+    const selected = index === actualMove;
+    const className = selected ? "move-notation selected" : "move-notation";
+    return { move, index, className, selected };
+}
+
+function createPairMoves(arr: string[], actualMove: number) {
+    const result: customMove[][] = [];
     for (let i = 0; i < arr.length; i += 2) {
         const first = arr[i];
         const second = arr[i + 1] !== undefined ? arr[i + 1] : null;
         result.push([
-            { m: first, i: i + 1 },
-            { m: second, i: i + 2 },
+            createCustomMove(first, i + 1, actualMove),
+            createCustomMove(second, i + 2, actualMove),
         ]);
     }
     return result;
 }
 
 function PanelMovesList() {
-    const { setActualMove, actualMove, movesHistory, colorWinner } =
+    const { setActualMove, actualMove, movesHistory, colorWinner, gameStatus } =
         useGameContext();
+
+    const isGameEnd = gameStatus === "gameEnd";
 
     const chessMoves = movesHistory.map((moveHistory) => {
         if (!moveHistory.lastMove) return "";
@@ -30,16 +49,23 @@ function PanelMovesList() {
 
     const panelMovesListRef = useRef<HTMLDivElement>(null);
     const selectedMoveRef = useRef<HTMLDivElement>(null);
+    const gameWinnerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        selectedMoveRef.current?.scrollIntoView({
+        let divElement = null;
+        if (gameWinnerRef.current && actualMove === movesHistory.length - 1) {
+            divElement = gameWinnerRef.current;
+        } else {
+            divElement = selectedMoveRef.current;
+        }
+        divElement?.scrollIntoView({
             behavior: "smooth",
             block: "nearest",
             inline: "center",
         });
-    }, [actualMove]);
+    }, [actualMove, movesHistory]);
 
-    const movesList = createPairMoves(chessMoves);
+    const movesList = createPairMoves(chessMoves, actualMove);
 
     return (
         <div className="panel-moves-list-wrapper">
@@ -49,42 +75,36 @@ function PanelMovesList() {
             <div className="panel-moves-list" ref={panelMovesListRef}>
                 {movesList.map((moves, index) => {
                     const lightRow = index % 2 === 1 ? " light-row" : "";
-                    const selected1 =
-                        moves[0].i === actualMove ? " selected" : "";
-                    const selected2 =
-                        moves[1].i === actualMove ? " selected" : "";
                     return (
                         <div key={index} className={"move-list-row" + lightRow}>
                             <div className="number">{index + 1}.</div>
                             <div
-                                ref={
-                                    moves[0].i === actualMove
-                                        ? selectedMoveRef
-                                        : null
-                                }
-                                className={"move-notation" + selected1}
-                                onClick={() => setActualMove(moves[0].i)}
+                                ref={moves[0].selected ? selectedMoveRef : null}
+                                className={moves[0].className}
+                                onClick={() => setActualMove(moves[0].index)}
                             >
-                                {moves[0].m}
+                                {moves[0].move}
                             </div>
-                            {moves[1].m ? (
+                            {moves[1].move ? (
                                 <div
                                     ref={
-                                        moves[1].i === actualMove
+                                        moves[1].selected
                                             ? selectedMoveRef
                                             : null
                                     }
-                                    className={"move-notation" + selected2}
-                                    onClick={() => setActualMove(moves[1].i)}
+                                    className={moves[1].className}
+                                    onClick={() =>
+                                        setActualMove(moves[1].index)
+                                    }
                                 >
-                                    {moves[1].m}
+                                    {moves[1].move}
                                 </div>
                             ) : null}
                         </div>
                     );
                 })}
-                {colorWinner ? (
-                    <div className="game-winner">
+                {isGameEnd ? (
+                    <div className="game-winner" ref={gameWinnerRef}>
                         {colorWinner === "w"
                             ? "1-0"
                             : colorWinner === "b"
