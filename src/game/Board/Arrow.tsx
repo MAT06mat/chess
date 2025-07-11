24afa94774569c6b7aa1aff195893b578b329600
Matthieu <matthieu.felten22@gmail.com>
2025-07-11 23:24:36 +0200

    feat: enhance Arrow component to calculate dynamic angles and lengths for improved rendering

--- src/game/Board/Arrow.tsx
+++ src/game/Board/Arrow.tsx
@@ -25,30 +25,74 @@ function Arrow({ from, to, color }: ArrowProps) {
     const [toX, toY] = positionToCoords(to, invertedColor);
     const dx = toX - x;
     const dy = toY - y;
-    const length = Math.sqrt(dx * dx + dy * dy);
-    const angle = Math.atan2(dy, dx) * (180 / Math.PI) + 90;
+
+    const absDx = Math.abs(dx);
+    const absDy = Math.abs(dy);
+
+    let angle = 0;
+    let points: number[][] = [];
+    if ((absDx === 12.5 && absDy === 25) || (absDx === 25 && absDy === 12.5)) {
+        if (dy === 25) {
+            angle = 180;
+        } else if (dx === 25) {
+            angle = 90;
+        } else if (dx === -25) {
+            angle = -90;
+        }
+
+        let xSign = 1;
+        const sameSign = dx * dy > 0;
+        if ((absDx === 12.5 && sameSign) || (absDy === 12.5 && !sameSign)) {
+            xSign = -1;
+        }
+
+        const vLenght = 25;
+        const hLenght = xSign * 12.5;
+        const arrow = {
+            halfWidth: 1.375,
+            halfHeadWidth: 3.25,
+            height: y - vLenght,
+            len: x + hLenght,
+            startHeight: 4.5,
+            headLen: xSign * 4.5,
+        };
+
+        points = [
+            [x - xSign * arrow.halfWidth, y - arrow.startHeight],
+            [x - xSign * arrow.halfWidth, arrow.height - arrow.halfWidth],
+            [arrow.len - arrow.headLen, arrow.height - arrow.halfWidth],
+            [arrow.len - arrow.headLen, arrow.height - arrow.halfHeadWidth],
+            [arrow.len, arrow.height],
+            [arrow.len - arrow.headLen, arrow.height + arrow.halfHeadWidth],
+            [arrow.len - arrow.headLen, arrow.height + arrow.halfWidth],
+            [x + xSign * arrow.halfWidth, arrow.height + arrow.halfWidth],
+            [x + xSign * arrow.halfWidth, y - arrow.startHeight],
+        ];
+    } else {
+        const length = Math.sqrt(dx * dx + dy * dy);
+        angle = Math.atan2(dy, dx) * (180 / Math.PI) + 90;
+
+        const arrow = {
+            halfWidth: 1.375,
+            halfHeadWidth: 3.25,
+            height: length,
+            startHeight: 4.5,
+            headHeight: length - 4.5,
+        };
+
+        points = [
+            [x - arrow.halfWidth, y - arrow.startHeight],
+            [x - arrow.halfWidth, y - arrow.headHeight],
+            [x - arrow.halfHeadWidth, y - arrow.headHeight],
+            [x, y - arrow.height],
+            [x + arrow.halfHeadWidth, y - arrow.headHeight],
+            [x + arrow.halfWidth, y - arrow.headHeight],
+            [x + arrow.halfWidth, y - arrow.startHeight],
+        ];
+    }
+
     const transform = `rotate(${angle} ${x} ${y})`;
-
-    const arrow = {
-        halfWidth: 1.375,
-        halfHeadWidth: 3.25,
-        height: length,
-        startHeight: 4.5,
-        headHight: length - 4.5,
-    };
-
-    const points = [
-        [x - arrow.halfWidth, y - arrow.startHeight],
-        [x - arrow.halfWidth, y - arrow.headHight],
-        [x - arrow.halfHeadWidth, y - arrow.headHight],
-        [x, y - arrow.height],
-        [x + arrow.halfHeadWidth, y - arrow.headHight],
-        [x + arrow.halfWidth, y - arrow.headHight],
-        [x + arrow.halfWidth, y - arrow.startHeight],
-    ]
-        .map((point) => point.join(" "))
-        .join(", ");
-
+    const formatedPoints = points.map((point) => point.join(" ")).join(", ");
     const arrowId = `arrow-${from}${to}`;
 
     let fill = "rgba(255, 170, 0, 0.8)";
@@ -62,34 +106,15 @@ function Arrow({ from, to, color }: ArrowProps) {
         fill: fill,
         opacity: 0.8,
     };
-    const className = "arrow";
+
     return (
         <polygon
             id={arrowId}
             data-arrow={from + to}
-            className={className}
-            points={points}
+            className={"arrow"}
+            points={formatedPoints}
             style={style}
             transform={transform}
-        />
-    );
-
-    return (
-        <polygon
-            id="arrow-e4c5"
-            data-arrow={from + to}
-            className="arrow"
-            transform="rotate(90 56.25 56.25) scale(-1, 1) translate(-112.5, 0)"
-            points="54.875 60.75,
-    54.875 82.625,
-    64.25 82.625,
-    64.25 84.5,
-    68.75 81.25,
-    64.25 78,
-    64.25 79.875,
-    57.625 79.875,
-    57.625 60.75"
-            style={{ fill: "rgba(255, 170, 0, 0.8)", opacity: 0.8 }}
         />
     );
 }
