c4ca4ba6944eecf4113d771f25602ad80e7ef427
Matthieu <matthieu.felten22@gmail.com>
2025-08-23 12:08:02 +0200

    fix: update BrowserRouter basename to conditionally handle localhost

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import "./index.scss";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 
+const local = window.location.host.includes("localhost");
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter basename="/chess/">
+        <BrowserRouter basename={local ? "" : "/chess/"}>
             <App />
         </BrowserRouter>
     </StrictMode>
